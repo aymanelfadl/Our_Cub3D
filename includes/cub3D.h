e8904666195fced3a24b9088875be1d4cb7b6cb6
Yasser Khoussi <ykhoussi@e1r1p3.1337.ma>
2025-11-03 18:32:50 +0100

    Expand config structs

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -5,7 +5,6 @@
 #include "../libft/libft.h"
 
 #include <stdio.h>
-#include <mlx.h>
 #include <math.h>
 
 #define TEXTURE_COUNT 4
@@ -48,6 +47,7 @@ typedef struct s_texture {
 typedef struct s_map {
     int   width;
     int   height;
+    int   sprite_count;
     char **grid;
 } t_map;
 
@@ -64,12 +64,18 @@ typedef struct s_player {
 typedef struct s_config
 {
     t_texture textures[TEXTURE_COUNT];
+    char      *sprite_path;
     t_color   floor_color;
     t_color   ceiling_color;
     t_map     map;
     t_player  player;
     int       have_floor;
     int       have_ceiling;
+    int       have_sprite;
+    int       have_player;
+    int       have_resolution;
+    int       resolution_width;
+    int       resolution_height;
 } t_config;
 
 
@@ -89,53 +95,7 @@ typedef struct s_game {
     t_config cfg;
 } t_game;
 
-// Main Parsing
-t_game *init_game(const char *file);
-void game_info(int fd, t_game *game);
-
-// Textures
-int is_texture(char *id);
-void which_identifier(t_texture *tex, char *key);
-t_texture get_texture(char **map);
-int game_config(t_game *game, char **map);
-
-// Colors
-char *merge_map_colors(char **map);
-int parse_color_value(char *str, t_color *color);
-void get_color_pointers(t_game *game, int is_floor, int **flag, t_color **color);
-void parse_apply_color(t_color *color, char **map);
-int handle_color(t_game *game, char **map, int is_floor);
-int in_range(int n, int min, int max);
-
-// Map
-int *get_map_dimension(const char *file);
-int valid_map(t_game *game);
-int game_map(t_game *game, char *line);
-void normalize_map(t_game *game);
-int scan_map_cells(t_game *game);
-int is_close_map(t_game *game);
-t_direction get_direction(t_game *game, char c);
-
-// Utils
-int ft_split_size(char **arr);
-int ft_strcmp(const char *s1, const char *s2);
-int wrap_ft_aoti(const char *str);
-char **allocate_map_grid(int height, int width);
-void print_err(const char *msg);
-void ft_free_split(char **arr);
-int check_extension(const char *file, const char *suffix);
-void check_duplicate(int *flag, int is_floor);
-int check_extension(const char *file, const char *suffix);
-int is_config(char *str);
-int is_valid_map_char(char c);
-int is_it_player(char c);
-
-
-// 
 void start_game(t_game *game);
-
-
-void debug_print_split(char **split, const char *original_line);
 
 
 #endif 
