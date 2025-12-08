5edb82a2ee1bd84c622730a616944df5cc4a5991
ayman elfadl <aymanelfadl16@gmail.com>
2025-12-08 15:26:34 +0100

    mok

--- main_bonus.c
+++ main_bonus.c
@@ -1,35 +1,18 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main_bonus.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: copilot <copilot@ai>                        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/30 21:40:00 by copilot           #+#    #+#             */
-/*   Updated: 2025/11/30 21:40:00 by copilot          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "cub3D.h"
 #include "parser_bonus.h"
-#include <stdio.h>
 
 int main(int ac, char *av[])
 {
-    if (ac != 2)
-    {
-        printf("Usage: %s <file.cub>\n", av[0]);
-        return 1;
-    }
-
     t_game game;
     t_parser_bonus parser;
     int code;
 
+    if (ac != 2)
+        return (printf("Usage: %s <file.cub>\n", av[0]), 1);
+
     ft_bzero(&game, sizeof(t_game));
     ft_bzero(&parser, sizeof(t_parser_bonus));
     parser.base.game = &game;
-
     code = parse_file_bonus(av[1], &parser);
     if (code != OK)
     {
@@ -37,23 +20,7 @@ int main(int ac, char *av[])
         parser_release_config(&game.cfg);
         return (1);
     }
-
-    game.mlx = mlx_init();
-    if (!game.mlx)
-    {
-        fprintf(stderr, "Error\nFailed to initialize MLX\n");
-        parser_release_config(&game.cfg);
-        return (1);
-    }
-
-    if (texture_load_all(game.mlx, &game.cfg) != 0)
-    {
-        fprintf(stderr, "Error\nFailed to load textures\n");
-        parser_release_config(&game.cfg);
-        return (1);
-    }
-
-    start_game(&game);
-    parser_release_config(&game.cfg);
+    if (!start_game(&game))
+        return (parser_release_config(&game.cfg), 1);
     return (0);
 }
