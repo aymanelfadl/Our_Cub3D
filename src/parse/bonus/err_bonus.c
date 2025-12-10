afccec65332e9902eac9d2168116630b6338352d
Yasser Khoussi <ykhoussi@c4r5p1.1337.ma>
2025-12-10 15:58:34 +0100

    last nor fix

--- src/parse/bonus/err_bonus.c
+++ src/parse/bonus/err_bonus.c
@@ -6,19 +6,29 @@
 /*   By: ykhoussi <ykhoussi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 15:41:05 by ykhoussi          #+#    #+#             */
-/*   Updated: 2025/12/10 15:41:06 by ykhoussi         ###   ########.fr       */
+/*   Updated: 2025/12/10 15:57:21 by ykhoussi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser_bonus.h"
 
+static char	*get_bonus_error(int code)
+{
+	if (code == ERR_INVALID_DOOR)
+		return ("Door placed in invalid position");
+	else if (code == ERR_DOOR_TEXTURE)
+		return ("Invalid door texture file");
+	else if (code == ERR_BONUS_CONFIG)
+		return ("Invalid bonus configuration");
+	return (NULL);
+}
+
 char	*get_bonus_error_msg(int code)
 {
-	static	char	*bonus_errors[] = {
-		[ERR_INVALID_DOOR] = "Door placed in invalid position",
-		[ERR_DOOR_TEXTURE] = "Invalid door texture file",
-		[ERR_BONUS_CONFIG] = "Invalid bonus configuration"};
-	if (code >= ERR_INVALID_DOOR && code <= ERR_BONUS_CONFIG)
-		return (bonus_errors[code - ERR_INVALID_DOOR]);
+	char	*msg;
+
+	msg = get_bonus_error(code);
+	if (msg)
+		return (msg);
 	return (get_error_msg(code));
 }
